5f8af13fc08802a544284ce30023ad7a398f0c3c
soumyaalur <atitbvb@gmail.com>
2021-03-31 11:54:02 -0500

    add one-to-many-association from backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
@@ -7,7 +8,4 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 25 },
                     format: {with: VALID_EMAIL_REGEX}
-
-            
-
 end
