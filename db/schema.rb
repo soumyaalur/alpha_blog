3064e0e11d9519039f6173fba65849e0816422f9
soumyaalur <nigudgis@gmail.com>
2021-05-08 11:11:10 -0500

    admin user intro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_193722) do
+ActiveRecord::Schema.define(version: 2021_05_08_160456) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_24_193722) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "avatar"
+    t.boolean "admin", default: false
   end
 
 end
