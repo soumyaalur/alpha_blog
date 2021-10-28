e9e308c57d323418cb0d5ce1973346fba52efeee
soumyaalur <nigudgis@gmail.com>
2021-10-28 12:44:24 -0500

    Create category from frontend

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,30 @@
 class CategoriesController < ApplicationController
 
   def new
-
+    @category = Category.new
   end
 
   def index
 
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was saved sucessfully"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
   def show
 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
